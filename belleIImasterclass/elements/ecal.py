a578f48f78a28e361c72587dbcc4cad880510b0f
Jonas Eppelt <jonas.eppelt@gmail.com>
2023-02-21 16:55:31 +0100

    refactoring of ecl

--- belleIImasterclass/elements/ecal.py
+++ belleIImasterclass/elements/ecal.py
@@ -5,32 +5,64 @@ import numpy as np
 
 
 from matplotlib.colors import to_rgba_array
-from matplotlib.collections import PatchCollection
 from matplotlib.patches import Rectangle
 
-class Crystal:
-    def __init__(self, x, y, content, edge_length, line_width):
-        self._x = x
-        self._y = y
-        self._content = content
-        self._edge_length = edge_length - line_width/ # account for line thickness for better visbility
-        self._line_width = line_width
-
-    def get_patch(self):
-        return Rectangle((self._x, self._y), self._edge_length, self._edge_length, linewidth = self._line_width)
-
 class ECal:
-    def __init__(self, crystal_edge_width = 0.5, noise_rate = 0, linewidth = 1.75) -> None:
-        self._crystal_edge_width = crystal_edge_width
-        self._noise_rate = noise_rate
+    def __init__(self, crystal_size = 4, linewidth = 1.75) -> None:
+        self._crystal_size = crystal_size
         self._linewidth = linewidth
+
         self._barrel_rows = 46
         self._barrel_columns = 144
-        self._barrel_crystal_patches = []
-        for c in range(self._barrel_columns):
-            for r in range(self._barrel_rows):
-                
+        self._barrel_center = np.array([0,-100])
+        barrel_coordinates, barrel_angles, barrel_offsets = self.get_barrel_patches
 
+        self._forward_center = np.array([-200,200])
+        self._forward_number_of_rings = 13
+        self._forward_rings_sizes = np.array([48,48,64,64,64,96,96,96,96,96,96,144,144])
+        self._forward_ring_radiuses = np.linspace(49.62,125.82,13)
+        forward_coordinates, forward_angles, forward_offsets = self.get_caps_patches(self._forward_center, self._forward_number_of_rings, self._forward_rings_sizes, self._forward_ring_radiuses, )
 
+        self._backward_center = np.array([200,200])
+        self._backward_number_of_rings = 10
+        self._backward_rings_sizes =  np.array([64,64,64,96,96,96,96,96,144,144])
+        self._backward_ring_radiuses = np.linspace(58.51,129.2,10)
+        backward_coordinates, backward_angles, backward_offsets = self.get_caps_patches(self._backward_center, self._backward_number_of_rings, self._backward_rings_sizes, self._backward_ring_radiuses, )
+
+        self._patch_coordinates = np.append(np.append(forward_coordinates, barrel_coordinates, axis = 1), backward_coordinates, axis=1)
+        self._patch_angles = np.append(np.append(forward_angles, barrel_angles), backward_angles)
+        self._patch_offsets = np.append(np.append(forward_offsets, barrel_offsets, axis = 1), backward_offsets, axis = 1)
+        
+        self._n_patches = 8736
+        self.patches = [Rectangle(
+            (self._patch_coordinates[:,n]+self._patch_offsets[:,n]),
+            width = self._crystal_size,
+            height = self._crystal_size,
+            angle = self._patch_angles[n]*180/np.pi,
+            linewidth = 20,
+        ) for n in range(self._n_patches)]
+    
     @property
-    def get
+    def get_barrel_patches(self) -> list[list[float]]:
+        x_positions = np.linspace(-np.pi*139.1, np.pi*139.1, self._barrel_columns)
+        y_positions = np.linspace(-153.6,153.6, 46)
+        coordinates = np.empty([2,0])
+        angles = np.empty([0])
+        offsets = np.empty([2,0])
+        for row in range(self._barrel_rows):
+            coordinates = np.append(coordinates, np.full((self._barrel_columns,2), self._barrel_center).T+np.array([x_positions, np.ones(self._barrel_columns)*y_positions[row]]), axis = 1)
+            angles = np.append(angles, np.zeros(144))
+            offsets = np.append(offsets, -np.ones([2,self._barrel_columns])*self._crystal_size, axis = 1)
+        return coordinates, angles, offsets
+    
+    def get_caps_patches(self, cap_center, number_of_rings, ring_sizes, ring_radiuses) -> list[list[float]]:
+
+        coordinates = np.empty([2,0])
+        angles = np.empty([0])
+        offsets = np.empty([2,0])
+        for n_ring in range(number_of_rings):
+            phi = np.linspace(0,2*np.pi, ring_sizes[n_ring], endpoint=False)
+            angles = np.append(angles, phi)
+            coordinates = np.append(coordinates, np.full((ring_sizes[n_ring], 2), cap_center).T+ring_radiuses[n_ring]*np.array([np.cos(phi), np.sin(phi)]), axis = 1)
+            offsets = np.append(offsets, -np.sqrt(2)*self._crystal_size*np.array([np.sin(np.pi/4-phi), np.cos(np.pi/4-phi)])/2, axis = 1)
+        return coordinates, angles, offsets
--- belleIImasterclass/widgets/clustering.py
+++ belleIImasterclass/widgets/clustering.py
@@ -1,11 +1,86 @@
+from matplotlib.path import Path
+from matplotlib.widgets import LassoSelector
 from belleIImasterclass.particlesmanager import ParticlesManager
+from belleIImasterclass.elements.ecal import ECal
+from belleIImasterclass.widgets.blitmanager import BlitManager
 from ipywidgets import Output, Accordion, Text, HBox, VBox
-
+import matplotlib.pyplot as plt
+import numpy as np
+from copy import deepcopy
+from matplotlib.collections import PatchCollection, LineCollection
 
 class ECLWidget:
     def __init__(self,particles_manager: ParticlesManager, noise_rate = 0.05) -> None:
         self._particles_manager = particles_manager
         self._noise_rate = noise_rate
+        self._ecal = ECal()        
+        self._selected_crystalls = [np.array([], dtype = int)]*self._particles_manager.n_particles
 
+        self._sel_particle = None
 
-        self._out = Output
+        self._crystall_colors = np.zeros((self._ecal._n_patches,4))
+        self._crystall_colors[:,0] = 1
+        for n_particle in range(len(self._particles_manager._df)):
+            self._crystall_colors[:,3] += self._particles_manager.get_crystall_content(n_particle)
+        self.noise = np.clip(np.random.normal(0,self._noise_rate, self._ecal._n_patches),0,10000)
+
+        self._crystall_colors[:,3] += self.noise
+        
+        self._crystall_content = deepcopy(self._crystall_colors[:,3])
+        self._crystall_colors[:,3] = np.clip(2*np.sqrt(self._crystall_colors[:,3]),0,1)
+        self._crystall_colors[np.where(self._crystall_colors[:,3]<=0.22),:]=[0,0,0,0.25]
+
+        self._out = Output()
+        with self._out:
+            self._fig, self._ax = plt.subplots(figsize = (14, 14*60/89), constrained_layout = True)
+        self._ax.set_ylim(-260, 340)
+        self._ax.set_xlim(-445,445)
+        self._ax.set_yticklabels([])
+        self._ax.set_xticklabels([])
+
+        self._crystall_collection = PatchCollection(self._ecal.patches, color = self._crystall_colors)
+        self._crystall_artist = self._ax.add_collection(self._crystall_collection)
+
+        self._selection_collection = LineCollection([])
+        self._selection_artist = self._ax.add_collection(self._selection_collection)
+        
+        self._blit_manager = BlitManager(self._fig.canvas, self._crystall_artist, self._selection_artist)
+
+        self._lasso = LassoSelector(self._ax, onselect = self.on_select)
+        
+        self._energy_labels = []
+        box_list = []
+        for i in range(self._particles_manager.n_particles):
+            self._energy_labels.append(Text(description = "Gesamte Energie der ausgewählten Kristalle in GeV:", value = "0", disabled=True))
+            box_list.append(HBox([self._energy_labels[i]]))
+        
+        self._particle_selector = Accordion(children=box_list,  titles = [f"Teilchen {str(i)}" for i in list(range(1,self._particles_manager.n_particles+1))], )
+        self._particle_selector.observe(self.change_particle, names="selected_index")
+        self._final_box = VBox(children=[self._particle_selector, self._out])
+    
+    def show(self) -> None:
+        with self._out:
+            plt.show()
+        display(self._final_box)
+        self.on_select()
+
+    def change_particle(self, change) -> None:
+        if self._particle_selector.selected_index is not None:
+            self._sel_particle = self._particle_selector.selected_index
+        else:
+            self._sel_particle = None
+        self.on_select()
+    
+    def on_select(self, verts = None) -> None:
+        if (verts is not None) and (self._sel_particle is not None):
+            print("verts")
+            path = Path(verts)
+            self._selected_crystalls[self._sel_particle] = np.nonzero(path.contains_points(self._ecal._patch_coordinates.T))[0]
+            energy = np.sum(self._crystall_content[abs(self._crystall_content[self._selected_crystalls[self._sel_particle]])])
+            self._particles_manager.energy_measurement(self._sel_particle, energy)
+            self._energy_labels[self._sel_particle].value = str(round(self.energies[self._sel_particle], 5))
+        edge_colors = np.clip(self._crystall_colors,0,1)
+        for n in range(self._particles_manager.n_particles):
+            edge_colors[self._selected_crystalls[n]] = [0,0,1,0.5] if n==self._sel_particle else [1,1,0,0.3]
+        self._crystall_artist.set_edgecolors(edge_colors)
+        self._blit_manager.update()
