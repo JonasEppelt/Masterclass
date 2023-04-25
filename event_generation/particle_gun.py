c488bf4aa01b1d8ca7a8e969a0349746d98853ca
Jonas Eppelt <jonas.eppelt@gmail.com>
2023-04-25 18:06:13 +0200

    work on better event generation

--- event_generation/particle_gun.py
+++ event_generation/particle_gun.py
@@ -18,236 +18,98 @@ try:
 except ImportError:
     # Output expected ImportErrors.
     print("Import Error")
+import gc
+from pathlib import Path
+import basf2
+from ROOT import Belle2
+import pyarrow as pa
+import pyarrow.parquet as pq
+import pandas as pd
+import polars as pl
+import time
+import os, psutil
+import numpy as np
+import matplotlib.pyplot as plt
+import b2luigi as luigi
 
-class getECLInfo(basf2.Module):
-    
-    def __init__(self, output, pdg, theta, phi, p, px, py, pz, pt, E):
+crystal_column_names = [
+    "reconstructed_energy",
+    "reconstructed_time",
+    "psd",
+    "fit_type",
+    "chi2_0",
+    "mc_energy_c0",
+    "mc_energy_c1",
+    "mc_energy_bkg",
+    "c0_fraction",
+    "c1_fraction",
+    "bkg_fraction",
+
+    ]
+
+class ECLInfoExtractor(basf2.Module):
+    def __init__(self):
         super().__init__()
-        self.output = output
-        self.obj_eclgeometrypar = Belle2.ECL.ECLGeometryPar.Instance()
-        self.barrel = np.arange(0, 8736, dtype=int)       #np.arange(1153, 7777, dtype=int)
-        self.col_names = [f'{i}' for i in self.barrel]
-        self.pdg = pdg
-        self.theta = theta
-        self.phi = phi
-        self.p = p
-        self.px = px
-        self.py = py
-        self.pz = pz
-        self.pt = pt
-        self.E = E
 
     def initialize(self):
-        self.eventinfo = Belle2.PyStoreObj('EventMetaData')
-        self.mcparticles = Belle2.PyStoreArray('MCParticles')
-        self.eclcaldigits = Belle2.PyStoreArray('ECLCalDigits')
-        self.eclclusters = Belle2.PyStoreArray('ECLClusters')
+        self.mc_particles = Belle2.PyStoreArray('MCParticles')
+        self.ecl_cal_digits = Belle2.PyStoreArray('ECLCalDigits')
+        self.b2_ecl_clusters = Belle2.PyStoreArray('ECLClusters')
         self.ecllocalmaxima = Belle2.PyStoreArray('ECLLocalMaximums')
-        
-        self.index = 0
+        self.eventinfo = Belle2.PyStoreObj('EventMetaData')
 
-        self.tot_data = pd.DataFrame()
+    def event(self) -> None:
+        uni_event_id = f"{self.eventinfo.getExperiment()}_{self.eventinfo.getRun()}_{self.eventinfo.getEvent()}"
+        df = pd.DataFrame()
 
-    def event(self):
-
-        correct_pdg = 0
-        correct_mass = 0
-        tot_energy = 0
-
-        energyinbarrel = False
-        ignore_event = False
-        correctpdg = False
-
-        self.is_dm = str(self.pdg).startswith("d")
-        if not self.is_dm:
-            self_pdg=int(self.pdg)
-        else:
-            self_pdg = self.pdg
-
-        print("in event: ", self.p, self.theta)
-
-        if len(self.mcparticles) == 1:
-            ignore_event = True
-
-        if not ignore_event:
-            
-
-            cells = dict.fromkeys(self.barrel, 0)
-
-            for caldigit in self.eclcaldigits:
-                rec_energy = caldigit.getEnergy()
-
-                ids = caldigit.getCellId()
-
-                if ids in cells:
-
-                    cells[ids] = rec_energy
-                    energyinbarrel = True
-            if energyinbarrel or self.is_dm:
-
-                for mc_idx, mc_particle in enumerate(self.mcparticles):
-                    mcrelations = mc_particle.getRelationsWith('ECLCalDigits')
-
-                    pdg = int(mc_particle.getPDG())
-                    print(pdg)
-                    print( pdg == self_pdg or self.is_dm, (pdg==self_pdg), pdg, self_pdg)
-
-                    if pdg == self_pdg or self.is_dm:
-                        correctpdg = True
-                        mass = mc_particle.getMass()
-                        energy = mc_particle.getEnergy()
-                        print(mcrelations.size())
-                        for mc_id in range(mcrelations.size()):
-                            # mc_energy = mcrelations.weight(mc_id)
-                            id = mcrelations.object(mc_id).getCellId()
-                            print(id in cells or self.is_dm)
-                            if id in cells or self.is_dm:
-                                correct_pdg = pdg
-                                correct_mass = mass
-                                tot_energy = energy
-
-                if correctpdg or self.is_dm:
+        # get mc particle information
+        for mc_particle_id, mc_particle in enumerate(self.mc_particles):
+            if mc_particle.getPDG() != 300553:
+                df[mc_particle_id] = pd.Series()
+                vector = mc_particle.get4Vector()
+                df.loc[mc_particle_id, "px"] = vector.Px()
+                df.loc[mc_particle_id, "py"] = vector.Py()
+                df.loc[mc_particle_id, "pz"] = vector.Pz()
+                df.loc[mc_particle_id, "energy"] = vector.P()
+                df.loc[mc_particle_id, "p"] = vector.E()
+                df.loc[mc_particle_id, "pt"] = vector.Pt()
+                df.loc[mc_particle_id, "theta"] = vector.Theta()
+                df.loc[mc_particle_id, "phi"] = vector.Phi()
+                df.loc[mc_particle_id, "pdg"] = mc_particle.getPDG()
+                df.loc[mc_particle_id, "mass"] = mc_particle.getMass()
+                df.loc[mc_particle_id, "charge"] = mc_particle.getCharge()
                 
-                    all_energy = [[e for e in cells.values()]]
-                    print(np.shape(np.array(all_energy)))
-                                
-                    data = pd.DataFrame(columns=self.col_names, data=all_energy)
-                    
-                    data['event'] = self.index
-                    data['pdg'] = correct_pdg
-                    data['mass'] = correct_mass
-                    data['energy'] = tot_energy
-                    data['theta'] = self.theta.item()
-                    data['phi'] = self.phi.item()
-                    data['p'] = self.p.item()
-                    data['px'] = self.px.item()
-                    data['py'] = self.py.item()
-                    data['pz'] = self.pz.item()
-                    data['pt'] = self.pt.item()
+                df.loc[mc_particle_id, [str(i+1)  for i in range(8735)]] = np.zeros(8735)
+            # get mc depositions
+                mc_relations = mc_particle.getRelationsWith("ECLCalDigits")
+                for mc_relations_id in range(mc_relations.size()):
+                    mc_energy = mc_relations.weight(mc_relations_id)
+                    cell_id = mc_relations.object(mc_relations_id).getCellId()
+                    df.loc[mc_particle_id, str(cell_id)] = mc_energy
 
-                    self.tot_data = pd.concat([self.tot_data, data])
-        self.index += 1
+        df.to_csv(f"{uni_event_id}.csv")
 
     def terminate(self):
-        
-        if len(self.tot_data>0):
-            print(self.tot_data['mass'])
-            self.tot_data = self.tot_data[self.tot_data['pdg']!=0]
-            print(self.tot_data)
-            self.tot_data.to_csv(self.output+'.csv')
-        else:
-            print("no matching events generated")
-            raise RuntimeError 
+        pass
 
-class EvtGenTask(Basf2PathTask):
-    name = luigi.Parameter()
-    index = luigi.Parameter()
-
-    batch_system = "local"
-
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.pdgs = events_dict[self.name]
-
-    @property
-    def input_dict(self):
-        self._input_dict = None
-        if self._input_dict is None:
-            self._input_dict = {k: v.path for d in self.input() if isinstance(d, dict) for k, v in d.items()}
-        return self._input_dict
-
-    def read_input(self):
-        input_key = f"{self.name}_fourvec_generation"
-        return pd.read_csv(self.input_dict[input_key], header = [0], index_col=0)
+if __name__ == "__main__":
+    main = basf2.create_path()
+    main.add_module('EventInfoSetter',
+            evtNumList=100)
+    
+    decfile = basf2.find_file('./decfile')
+    main.add_module('EvtGenInput', userDECFile=decfile)
     
 
-    def get_log_file_dir(self):
-        path = os.path.join(base_path, "particle_gun", self.name, str(self.index))
-        os.makedirs(path, exist_ok=True)
-        return path
+    main.add_module('Gearbox')
+    main.add_module('Geometry', useDB=True)
 
-    def create_path(self):
-        df = self.read_input()
-        particle = df.iloc[self.index]
-        print(particle)
-        is_dm =  str(particle['pdg']).startswith('d')
+    add_simulation(path=main)
 
-        converted_pdg = 22 if is_dm else int(particle['pdg']) # use gamma for simulating DM and setting to 0 later
-        
-        
+    add_reconstruction(path=main)
 
-        main = basf2.create_path()
-        particlegun = basf2.register_module('ParticleGun')
+    ecl_output = ECLInfoExtractor()
 
-        particlegun.param('pdgCodes', [converted_pdg])
-
-        particlegun.param('momentumGeneration', 'fixed')
-        particlegun.param('momentumParams', [particle['p']])
-
-        particlegun.param('thetaGeneration', 'fixed')
-        particlegun.param('thetaParams', [particle['theta']*180/np.pi])
-
-        particlegun.param('phiGeneration', 'fixed')
-        particlegun.param('phiParams', [particle['phi']*180/np.pi])
-
-        main.add_module(particlegun)
-
-        main.add_module('EventInfoSetter',
-                        evtNumList=50)
-
-        main.add_module('Gearbox')
-        main.add_module('Geometry', useDB=True)
-
-        add_simulation(path=main)
-
-        add_reconstruction(path=main)
-    
-        ecl_output = getECLInfo(self.output_file(),
-                                particle['pdg'],
-                                particle['theta'],
-                                particle['phi'],
-                                particle['p'],
-                                particle['px'],
-                                particle['py'],
-                                particle['pz'],
-                                particle['pt'],
-                                particle['E']
-                                )
-
-        main.add_module(ecl_output)
-
-        return main
-
-    def process(self):
-        try:
-            import basf2
-            import ROOT
-        except ImportError:
-            raise ImportError("Cannot find basf2 or ROOT")
-
-        path = self.create_path()
-
-        path.add_module('Progress')
-        #basf2.print_path(path)
-        basf2.process(path)
-
-        #print(basf2.statistics)
-    
-    @property
-    def output_path(self):
-        return os.path.join(base_path, "particle_gun")
-    
-    def output_file(self):
-        return os.path.join(self.output_path, self.name, f"{self.index}")
-
-    
-    def output(self):
-        output_key = f"{self.name}_{self.index}_particle_gun"
-        return {output_key: luigi.LocalTarget(f"{self.output_file()}.csv")}
-
-
-    def requires(self):
-        yield FourVecGenTask(
-            name = self.name
-        )
+    main.add_module(ecl_output)
+    main.add_module('Progress')
+    basf2.process(main)
